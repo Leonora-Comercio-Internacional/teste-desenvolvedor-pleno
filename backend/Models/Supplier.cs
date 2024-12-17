6ab55acf6a2ad037195040c6215663ed934dfacc
Thalesmau <thalesmausiq46@gmail.com>
2024-12-17 00:35:59 -0300

    feat(models): adding models for category, product, supplier, and product_supplier

---
+++ backend/Models/Supplier.cs
@@ -0,0 +1,11 @@
+﻿namespace backend.Models;
+
+public class Supplier
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? CNPJ { get; set; }
+    public string? Telephone { get; set; }
+    public string? Address { get; set; }
+    public DateTime? DateAdded { get; set; }
+}
